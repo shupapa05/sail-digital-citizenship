2b3eb4e7c23825af8a472b9be80b20802d549702
shupapa05 <rotc41yh@gmail.com>
2026-05-03 19:38:19 +0900

    Add Supabase config

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://mmczwvzkvpabsokwqqho.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_QWhO3irnQPyHBOT4z5AmoQ_z9_vGast';
+export const STORAGE_BUCKET = 'sail-proofs';
